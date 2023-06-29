3c90f2b07e97fa06f3bd7b68a97c51f9244b8701
Sean <zhangshuyan@me.com>
2023-06-29 21:07:00 +1000

    Update revenuecat_beta.js

--- revenuecat/revenuecat_beta.js
+++ revenuecat/revenuecat_beta.js
@@ -2,7 +2,6 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 let userAgent = navigator.userAgent;
-let appName;
 
 if(userAgent.match(.*MySwimPro.*){
   obj.subscriber.entitlements = {
@@ -31,31 +30,8 @@ if(userAgent.match(.*MySwimPro.*){
       }
 }
 
-obj.subscriber.entitlements = {
-      "co.bergen.Darkroom.entitlement.allToolsAndFilters": {
-        "expires_date": "2099-07-15T05:14:16Z",
-        "grace_period_expires_date": null,
-        "product_identifier": "co.bergen.Darkroom.product.year.everything",
-        "purchase_date": "2022-07-08T05:14:16Z"
-      },
-      "co.bergen.Darkroom.entitlement.selectiveAdjustments": {
-        "expires_date": "2099-07-15T05:14:16Z",
-        "grace_period_expires_date": null,
-        "product_identifier": "co.bergen.Darkroom.product.year.everything",
-        "purchase_date": "2022-07-08T05:14:16Z"
-      },
-      "premium": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2023-06-27T15:32:50Z",
-        "product_identifier": "com.neybox.pillow.premium.year",
-        "expires_date": "2099-07-15T05:14:16Z"
-      },
-      "Royale": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2023-06-27T15:39:30Z",
-        "product_identifier": "com.samvermette.Transit.royale.group2.annual",
-        "expires_date": "2099-07-15T05:14:16Z"
-      },
+if(userAgent.match(.*Spark.*){
+    obj.subscriber.entitlements = {
       "premium": {
         "grace_period_expires_date": null,
         "purchase_date": "2023-06-27T17:08:00Z",
@@ -69,48 +45,8 @@ obj.subscriber.entitlements = {
         "expires_date": "2099-06-18T10:26:37Z"
       }
     },
-  
-obj.subscriber.subscriptions = {
-      "co.bergen.Darkroom.product.year.everything": {
-        "billing_issues_detected_at": null,
-        "expires_date": "2099-07-15T05:14:16Z",
-        "grace_period_expires_date": null,
-        "is_sandbox": false,
-        "original_purchase_date": "2022-07-08T05:14:17Z",
-        "ownership_type": "PURCHASED",
-        "period_type": "normal",
-        "purchase_date": "2022-07-08T05:14:16Z",
-        "store": "app_store",
-        "unsubscribe_detected_at": null
-      },
-      "com.neybox.pillow.premium.year": {
-        "original_purchase_date": "2023-06-27T15:32:51Z",
-        "expires_date": "2099-07-15T05:14:16Z",
-        "is_sandbox": false,
-        "refunded_at": null,
-        "auto_resume_date": null,
-        "grace_period_expires_date": null,
-        "period_type": "normal",
-        "purchase_date": "2023-06-27T15:32:50Z",
-        "billing_issues_detected_at": null,
-        "ownership_type": "PURCHASED",
-        "store": "app_store",
-        "unsubscribe_detected_at": null
-      },
-      "com.samvermette.Transit.royale.group2.annual": {
-        "original_purchase_date": "2023-06-27T15:39:31Z",
-        "expires_date": "2099-07-15T05:14:16Z",
-        "is_sandbox": false,
-        "refunded_at": null,
-        "auto_resume_date": null,
-        "grace_period_expires_date": null,
-        "period_type": "trial",
-        "purchase_date": "2023-06-27T15:39:30Z",
-        "billing_issues_detected_at": null,
-        "ownership_type": "PURCHASED",
-        "store": "app_store",
-        "unsubscribe_detected_at": null
-      },
+
+  obj.subscriber.subscriptions = {
       "spark_b_4199_1y_1w0": {
         "original_purchase_date": "2022-10-06T08:29:18Z",
         "expires_date": "2099-10-01T11:39:07Z",
